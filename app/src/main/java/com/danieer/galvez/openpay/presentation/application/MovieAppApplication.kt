f3e86c2cc8ddd333a4209a73f4046c67ab7d1d59
Danieer Galvez <Moldycord@gmail.com>
2023-11-13 16:22:54 -0500

    Finished show users location on a map

--- app/src/main/java/com/danieer/galvez/openpay/presentation/application/MovieAppApplication.kt
+++ app/src/main/java/com/danieer/galvez/openpay/presentation/application/MovieAppApplication.kt
@@ -3,6 +3,7 @@ package com.danieer.galvez.openpay.presentation.application
 import android.app.Application
 import androidx.fragment.app.Fragment
 import com.danieer.galvez.openpay.presentation.di.DaggerAppComponent
+import com.google.firebase.FirebaseApp
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -23,6 +24,7 @@ class MovieAppApplication : Application(), HasAndroidInjector, HasSupportFragmen
             .application(this)
             .build()
             .inject(this)
+        FirebaseApp.initializeApp(this)
     }
 
     override fun androidInjector(): AndroidInjector<Any> {
--- app/src/main/java/com/danieer/galvez/openpay/presentation/ui/fragments/MapFragment.kt
+++ app/src/main/java/com/danieer/galvez/openpay/presentation/ui/fragments/MapFragment.kt
@@ -1,6 +1,139 @@
 package com.danieer.galvez.openpay.presentation.ui.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
+import com.danieer.galvez.openpay.databinding.FragmentMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var gMap: GoogleMap
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMapBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.apply {
+            onCreate(savedInstanceState)
+        }
+        verifyLocationPermission()
+    }
+
+    private fun verifyLocationPermission() {
+        if (checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PermissionChecker.PERMISSION_GRANTED && checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PermissionChecker.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            binding.mapView.getMapAsync(this)
+            enableLocation()
+        }
+
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        gMap = p0
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            gMap.isMyLocationEnabled = true
+        }
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    binding.mapView.getMapAsync(this@MapFragment)
+                    enableLocation()
+                }
+            }
+        }
+    }
+
+    private fun enableLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                p0.lastLocation?.let {
+                    updateMapLocation(it.latitude, it.longitude)
+
+                }
+            }
+        }
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 10000
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest, locationCallback, Looper.getMainLooper()
+            )
+        }
+
+
+    }
+
+    private fun updateMapLocation(latitude: Double, longitude: Double) {
+        val currentLatLng = LatLng(latitude, longitude)
+        gMap.addMarker(MarkerOptions().position(LatLng(latitude, longitude)).title("Your Location"))
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+        gMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
 }
