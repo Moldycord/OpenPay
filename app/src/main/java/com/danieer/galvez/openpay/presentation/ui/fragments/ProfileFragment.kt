002b18d3e7ea20709390a2b4ac8e9d075a02d3aa
Danieer Galvez <Moldycord@gmail.com>
2023-11-14 12:58:53 -0500

    Finished  layout

--- app/src/main/java/com/danieer/galvez/openpay/presentation/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/danieer/galvez/openpay/presentation/ui/fragments/ProfileFragment.kt
@@ -1,6 +1,21 @@
 package com.danieer.galvez.openpay.presentation.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.danieer.galvez.openpay.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
 }
